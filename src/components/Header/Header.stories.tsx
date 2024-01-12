8d02c6d012ab9b05083f29076c0dc74dda6e7cbd
kostal79 <kostal1979@gmail.com>
2024-01-12 14:03:58 +0300

    story updated

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -1,17 +1,29 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import Header from './Header';
+import type { Meta, StoryObj } from "@storybook/react";
+import Header from "./Header";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import React from "react";
 
 const meta: Meta<typeof Header> = {
-title: 'Header/Header',
-component: Header,
-parameters: {
-  layout: 'centered'
-},
-tags: ['autodocs'],
+  title: "Header/Header",
+  component: Header,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 };
 
 export default meta;
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
 export const HeaderStory: Story = {
+  render: function Render() {
+    return (
+      <Provider store={store}>
+        <div style={{ width: "600px" }}>
+          <Header />
+        </div>
+      </Provider>
+    );
+  },
 };
--- src/ui/audio-player/AudioPlayer.stories.tsx
+++ src/ui/audio-player/AudioPlayer.stories.tsx
@@ -1,22 +1,32 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import AudioPlayer from './AudioPlayer';
-
-
+import type { Meta, StoryObj } from "@storybook/react";
+import AudioPlayer from "./AudioPlayer";
+import { Provider } from "react-redux";
+import { store } from "../../store/store";
+import React from "react";
 
 const meta: Meta<typeof AudioPlayer> = {
-title: 'AudioPlayer/AudioPlayer',
-component: AudioPlayer,
-parameters: {
-  layout: 'centered'
-},
-tags: ['autodocs'],
+  title: "AudioPlayer/AudioPlayer",
+  component: AudioPlayer,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
 };
 
 export default meta;
-type Story = StoryObj<typeof meta>
+type Story = StoryObj<typeof meta>;
 
 export const AudioPlayerStory: Story = {
   args: {
-    track: 'https://dl2.mp3party.net/online/586606.mp3'
-  }
+    record: "",
+    partnership_id: "",
+    id: "",
+  },
+  render: function Render(args) {
+    return (
+      <Provider store={store}>
+        <AudioPlayer {...args} />
+      </Provider>
+    );
+  },
 };
